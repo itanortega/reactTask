597b2198f56948d7abc733fce776868085f06781
Itan Ortega <io@MacBook-Air-de-Itan.local>
2018-09-08 05:23:16 -0500

    eventos

--- myfistapp/src/App.js
+++ myfistapp/src/App.js
@@ -3,19 +3,52 @@ import logo from './logo.svg';
 import './App.css';
 
 import { todos } from './todos.json';
+import TodoForm from './components/TodoForm';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       todos
-    }
+    };
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+  
   render() {
     const todos = this.state.todos.map((todo, i) => {
       return (
-        <div className="card">
-          { todo.title }
+        <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+              <h3>{ todo.title }</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                { todo.priority }
+              </span>
+            </div>
+            <div className="card-body">
+              <p>{ todo.description }</p>
+              <p><mark>{ todo.responsible }</mark></p>
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Eliminar
+              </button>
+            </div>
+          </div>
         </div>
       )
     })
@@ -24,11 +57,26 @@ class App extends Component {
       <div className="App">
         <nav className="navbar navbar-dark bg-dark">
           <a href="" className="text-white">
-              Tareas 
+              Tareas
+              <span className="badge badge-pill badge-light ml-2">
+                { this.state.todos.length }
+              </span>
           </a>
         </nav>
-        { todos }
-        <img src={logo} className="App-logo" alt="logo" />
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-md-4 text-center">
+              <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
+            </div>
+
+            <div className="col-md-8">
+              <div className="row">
+                {todos}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
